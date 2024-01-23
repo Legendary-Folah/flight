41e540409b212335ea7a35f833530d3398f5da24
FolaDev <folarinfadeyi@gmail.com>
2024-01-23 18:22:06 +0100

    changes

--- lib/core/widgets/flight_text_form_field.dart
+++ lib/core/widgets/flight_text_form_field.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class FlightTextFormField extends StatelessWidget {
   const FlightTextFormField(
       {super.key,
+        this.keyboardType,
+        this.textInputFormatter,
         required TextEditingController titleController,
         required this.labelText})
       : _titleController = titleController;
 
+  final List<TextInputFormatter>? textInputFormatter;
+  final TextInputType? keyboardType;
   final TextEditingController _titleController;
   final String labelText;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: _titleController,
-      decoration: InputDecoration(
-        labelText: labelText,
-        contentPadding: const EdgeInsets.all(10.0),
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextFormField(
+        controller: _titleController,
+        inputFormatters: textInputFormatter,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          labelText: labelText,
+          contentPadding: const EdgeInsets.all(10.0),
+        ),
       ),
     );
   }
